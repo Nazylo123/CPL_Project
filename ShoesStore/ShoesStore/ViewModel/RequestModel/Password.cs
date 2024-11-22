049791e15bfb4547f5d5917ac46f851ef037e9e5
seva2103 <minhnvhe172822@fpt.edu.vn>
2024-11-22 13:27:01 +0700

    update authcontroller

---
+++ ShoesStore/ShoesStore/ViewModel/RequestModel/Password.cs
@@ -0,0 +1,18 @@
+﻿namespace ShoesStore.ViewModel.RequestModel
+{
+    public class Password
+    {
+    }
+    public class ForgotPasswordRequestModel
+    {
+        public string Email { get; set; }
+    }
+
+    public class ResetPasswordRequestModel
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+    }
+
+}
