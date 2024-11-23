e62e23d9c6153a99832d281d69308b0550b64266
seva2103 <minhnvhe172822@fpt.edu.vn>
2024-11-23 19:31:32 +0700

    merge minh03

--- ShoesStore/ShoesStore/obj/Debug/net8.0/ShoesStore.AssemblyInfo.cs
+++ ShoesStore/ShoesStore/obj/Debug/net8.0/ShoesStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoesStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+049791e15bfb4547f5d5917ac46f851ef037e9e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9333eaf132116ee2991d74cecaccc800782a2f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoesStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoesStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
